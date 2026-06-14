cd7757261da9f56ee185d96ef7295f2698c314a7
ogubuikealex <ogubuikealex@gmail.com>
2026-06-14 12:46:42 +0100

    feat: set up database

---
+++ backend/db/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from .models import Base
+from config import settings
+
+engine = create_async_engine(settings.database_url, echo=False, pool_size=10)
+AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_session():
+    async with AsyncSessionLocal() as session:
+        yield session
